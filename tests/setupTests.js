e70862180cadac2b05e7b7b8eaed7aed1e9724fd
Eric Kizaki <erickizaki@gmail.com>
2018-08-06 18:58:45 -0500

    master mock out axios

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -5,3 +5,14 @@ import Adapter from "enzyme-adapter-react-16";
 require("react-native-mock-render/mock"); // <-- side-effects!!!
 
 Enzyme.configure({ adapter: new Adapter() });
+
+jest.mock("axios", () => {
+  const mockAxios = {
+    post: jest.fn().mockImplementation((url, obj) => Promise.resolve({})),
+    get: jest.fn().mockImplementation(url => Promise.resolve({})),
+    put: jest.fn().mockImplementation((url, obj) => Promise.resolve({})),
+    delete: jest.fn().mockImplementation(url => Promise.resolve({}))
+  };
+
+  return mockAxios;
+});
